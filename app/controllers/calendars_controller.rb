aa9116b5a4548a85b344b5d9cfac69ab466dd6b6
nocchicken <pta624yeaaah@gmail.com>
2023-10-02 16:36:18 +0900

    コンフリクト(first-second)

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan, :calendars)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -35,11 +35,11 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       wday_num = (@todays_date + x).wday# wdayメソッドを用いて取得した数値
-      if wday_num <= 7#「wday_numが7以上の場合」という条件式
+      if wday_num >= 7#「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
 
